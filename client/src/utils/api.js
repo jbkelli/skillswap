97efb01bac4bd73ea71c50768f2c5e60df40d675
jbkelli <444mwangialvinm@gmail.com>
2025-09-02 00:51:55 +0300

    updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 // client/src/utils/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://skillswap-jfq6.onrender.com/';
 
 // Create axios instance
 const api = axios.create({
